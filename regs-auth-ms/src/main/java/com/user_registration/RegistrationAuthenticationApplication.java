f733bdcfe24a07b230afe0b7dc9856f9a20f52c5
victorferreiras <victor.fagundes586@gmail.com>
2023-06-22 19:59:49 -0300

    implemented eureka clients

--- regs-auth-ms/src/main/java/com/user_registration/RegistrationAuthenticationApplication.java
+++ regs-auth-ms/src/main/java/com/user_registration/RegistrationAuthenticationApplication.java
@@ -4,8 +4,10 @@ import com.user_registration.user.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @AllArgsConstructor
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RegistrationAuthenticationApplication {
 
