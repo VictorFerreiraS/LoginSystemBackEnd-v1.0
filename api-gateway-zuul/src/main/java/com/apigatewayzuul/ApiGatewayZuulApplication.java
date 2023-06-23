d452d121cf435828e57fed11096ef025543d68d4
victorferreiras <victor.fagundes586@gmail.com>
2023-06-23 10:36:49 -0300

    created api gateway zuul

---
+++ api-gateway-zuul/src/main/java/com/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayZuulApplication.class, args);
+	}
+
+}
