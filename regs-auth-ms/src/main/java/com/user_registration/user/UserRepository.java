44462701b9f94025e4abe5b0ecb2dba4e2c8b620
victor <victor.fagundes586@gmail.com>
2023-08-14 20:48:30 -0300

    added change password custom query

--- regs-auth-ms/src/main/java/com/user_registration/user/UserRepository.java
+++ regs-auth-ms/src/main/java/com/user_registration/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.user_registration.user;
 
+import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +12,13 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :userId")
+    void updatePassword(@Param("userId") int userId, @Param("newPassword") String newPassword);
+
+    @Modifying
+    @Query("UPDATE User u SET u.password = :newPassword WHERE u.id = :userId AND u.password = :oldPassword")
+    int updatePasswordWithOld(@Param("userId") int userId, @Param("oldPassword") String oldPassword, @Param("newPassword") String newPassword);
 }
+
