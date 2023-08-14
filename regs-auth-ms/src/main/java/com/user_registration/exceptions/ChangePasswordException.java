9fe1d63b28102f94cde0b4813727c57b7d0db446
victor <victor.fagundes586@gmail.com>
2023-08-14 20:47:19 -0300

    custom exception created

---
+++ regs-auth-ms/src/main/java/com/user_registration/exceptions/ChangePasswordException.java
@@ -0,0 +1,8 @@
+package com.user_registration.exceptions;
+
+public class ChangePasswordException extends Exception{
+    public ChangePasswordException(String message) {
+        super(message);
+    }
+
+}
