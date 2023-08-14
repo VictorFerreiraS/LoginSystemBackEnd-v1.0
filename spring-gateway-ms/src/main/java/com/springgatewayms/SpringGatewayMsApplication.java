62572038f356e62b62f6158e41e108ab0ae795ce
victor <victor.fagundes586@gmail.com>
2023-08-14 19:16:39 -0300

    default user

--- spring-gateway-ms/src/main/java/com/springgatewayms/SpringGatewayMsApplication.java
+++ spring-gateway-ms/src/main/java/com/springgatewayms/SpringGatewayMsApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SpringGatewayMsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringGatewayMsApplication.class, args);
     }
