3372e6d2ea2a4d2242b29431259f8fa7428faa07
victor <victor.fagundes586@gmail.com>
2023-08-27 19:00:06 -0300

    fixed wrong import statement

--- regs-auth-ms/src/main/java/com/user_registration/user/UserRepository.java
+++ regs-auth-ms/src/main/java/com/user_registration/user/UserRepository.java
@@ -1,9 +1,9 @@
 package com.user_registration.user;
 
-import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
