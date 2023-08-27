1b5f1a333d1f007537210eede21a6bbfddb1bde4
victor <victor.fagundes586@gmail.com>
2023-08-27 18:58:31 -0300

    created request

---
+++ regs-auth-ms/src/main/java/com/user_registration/requests/ChangePasswordRequest.java
@@ -0,0 +1,21 @@
+package com.user_registration.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordRequest {
+    private String oldPassword;
+    private String newPassword;
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
